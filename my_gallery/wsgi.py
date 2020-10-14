bd63689e626e0304792bd880f0fe1fa85dc37a18
PhoebeNjeri <Phoebegichuhi4@gmail.com>
2020-10-14 16:23:59 +0300

    all changes

--- my_gallery/wsgi.py
+++ my_gallery/wsgi.py
@@ -11,5 +11,5 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gallery.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_gallery.settings')
 application = get_wsgi_application()
